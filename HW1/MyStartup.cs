eb6bbc86817f0f2a3cb93294f5c277be30330d85
yevtayev <yevtayev@gmail.com>
2020-12-13 21:19:54 +0200

    HW1_Ver:1

---
+++ HW1/MyStartup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+
+namespace HW1
+{
+    public class MyStartup
+    {
+        private IConfiguration _configuration { get; set; }
+        public MyStartup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync(_configuration["Logging:LogLevel:Microsoft.Hosting.Lifetime"]);
+                });
+            });
+        }
+    }
+}
