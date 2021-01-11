10bd05d9e235a48da4360056a1e1152e210b0b73
Dileon <yevtayev@gmail.com>
2021-01-11 13:17:40 +0200

    Removing all files in .gitignore

---
+++ HW4/Controllers/HumanController.cs
@@ -0,0 +1,28 @@
+﻿using HW4.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HW4.Controllers
+{
+    public class HumanController : Controller
+    {
+        private InfestationDbContext _context { get; }
+
+        public HumanController(InfestationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Humans"] = _context.Humans.ToList();
+            return View();
+        }
+
+    }
+}
---
+++ HW4/Models/InfestationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace HW4.Models
+{
+    public class InfestationDbContext : DbContext
+    {
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Human> Humans { get; set; }
+        private IConfiguration _configuration { get; }
+        public InfestationDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSQLServer
+            optionsBuilder.UseMySql(_configuration.GetConnectionString("InfestationDbConnectionNew"));
+        }
+    }
+}
